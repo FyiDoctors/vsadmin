9eb9a95f10a2915f208877847dd31fbd2601275d
Dean Skelton <dean.skelton@fyidoctors.com>
2014-01-20 09:50:20 -0700

    fixed reports

--- app/models/membership_fee.rb
+++ app/models/membership_fee.rb
@@ -10,10 +10,10 @@ class MembershipFee < ActiveRecord::Base
   def self.to_csv
     CSV.generate do |csv|
 #      csv << column_names
-      csv << ["Submit Id", "Vision Source Id", "Vendor Id" ,"Clinic Name", "Address", "Month", "Year", "Receipts", "Credit Card", "Refunds", "Tax", "Fees"]
+      csv << ["Submit Id", "Vision Source Id", "Vendor Id" ,"Clinic Name", "Address", "Month", "Year", "Receipts", "Credit Card", "Refunds", "Tax", "Fees", "Date Submitted"]
       all.each do |fee|
 #        csv << fee.attributes.values_at(*column_names)
-        csv << [fee.id, fee.clinic.vsid, fee.clinic.vendorid, fee.clinic.name, fee.clinic.address, fee.month, fee.year, fee.receipts, fee.creditcard, fee.refunds, fee.tax, fee.fee]
+        csv << [fee.id, fee.clinic.vsid, fee.clinic.vendorid, fee.clinic.name, fee.clinic.address, fee.month, fee.year, fee.receipts, fee.creditcard, fee.refunds, fee.tax, fee.fee, fee.created_at]
       end
     end
   end
