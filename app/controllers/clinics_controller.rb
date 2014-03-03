4ebbe86a248ebd88e5aec80feda6ca025d4009e4
Dean Skelton <dean.skelton@fyidoctors.com>
2014-03-03 11:20:54 -0700

    final alterations

--- app/controllers/clinics_controller.rb
+++ app/controllers/clinics_controller.rb
@@ -70,7 +70,7 @@ class ClinicsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def clinic_params
-      params.require(:clinic).permit(:name, :doctor, :address, :vsid, :vsvendorid)
+      params.require(:clinic).permit(:name, :address, :vendorid, :admin)
     end
     
     
